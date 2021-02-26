21b7967fc6536a60f1a29810e6bd2d9b85ecf8ad
ars <ion8@bk.ru>
2021-02-26 18:37:42 +0300

    upd text, content

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <ProductArea productsData={productsData} />
       <footer className='globalFooter'>
         <span>E-mail: info@revolna.com</span>
+        <p>*in Pro version</p>
         <div className='socialNetworks-wrapper'>
           <a href='https://twitter.com/revolnaenergy'>
             @revolnaenergy
